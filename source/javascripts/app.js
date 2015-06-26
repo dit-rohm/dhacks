a5780107eb91f822e0681c3fc3cfc4ba6c77771c
Keita Nakanishi <nakanishy@yahoo.com>
2015-06-26 23:39:19 +0900

    いろいろ追加
    ・曜日間違え修正
    ・細かいデザイン修正
    ・スケジュール追加

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,22 +1,2 @@
 $(function() {
-  // map表示
-  var latlng = new google.maps.LatLng(34.802760, 135.771273);
-  var options = {
-    zoom: 15,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    scrollwheel: false
-  };
-  var map = new google.maps.Map(document.getElementById('map'), options);
-  var styles =
-  [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#4694ec"},{"visibility":"on"}]}];
-
-  map.setOptions({styles: styles});
-
-  // マーカーの表示
-  var marker = new google.maps.Marker({
-    position: map.getCenter(),
-    title: 'ローム記念館',
-    map: map
-  });
 });
