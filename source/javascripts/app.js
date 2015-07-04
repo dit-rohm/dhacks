2ffde9ff3e7d292e347ebdb7c94263260ef1326a
chikam <c.coco.102@gmail.com>
2015-07-04 16:01:41 +0900

    ソーシャルボタンの追加 #70

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,2 +1,10 @@
 $(function() {
+  ;(function(w,d){
+    w.___gcfg = {lang:"ja"};
+    var s,e = d.getElementsByTagName("script")[0],
+    a = function(u,f){if(!d.getElementById(f)){s = d.createElement("script");
+      s.src = u;if(f){s.id = f;}e.parentNode.insertBefore(s,e);}};
+    a("//platform.twitter.com/widgets.js","twitter-wjs");
+    a("//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0","facebook-jssdk");
+  })(this, document);
 });
